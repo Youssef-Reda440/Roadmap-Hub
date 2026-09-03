a906c073bcb5c496963990b6432d96af337db380
Youssef Reda <youssefreda866@gmail.com>
2026-09-03 11:44:08 +0300

    feat: add public controllers structure

---
+++ app/Http/Controllers/Public/CreatorProfileController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Public;
+
+use App\Http\Controllers\Controller;
+
+class CreatorProfileController extends Controller
+{
+    public function show()
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Public/RoadmapController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Public;
+
+use App\Http\Controllers\Controller;
+
+class RoadmapController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function show()
+    {
+        //
+    }
+}
