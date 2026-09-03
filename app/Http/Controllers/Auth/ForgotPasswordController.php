232e1f24a377194bea1692d8a2ac993772f80ae1
Youssef Reda <youssefreda866@gmail.com>
2026-09-03 11:44:25 +0300

    feat: add auth controllers structure

---
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+
+class ForgotPasswordController extends Controller
+{
+    public function show()
+    {
+        //
+    }
+
+    public function sendResetLink()
+    {
+        //
+    }
+}
