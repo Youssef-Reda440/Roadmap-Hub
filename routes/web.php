c478f5023d8513e70ba319b1d7e75ad67e05fc98
Youssef Reda <youssefreda866@gmail.com>
2026-09-03 11:47:19 +0300

    refactor: organize web routes by module

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+require __DIR__ . '/public.php';
+require __DIR__ . '/auth.php';
+require __DIR__ . '/learner.php';
+require __DIR__ . '/creator.php';
+require __DIR__ . '/admin.php';
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('roadmap-hub');
 });
