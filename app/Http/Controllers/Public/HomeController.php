eef5d5176ecfbd6b9a3f9b141b75a2f710d0dd3c
Youssef Reda <youssefreda866@gmail.com>
2026-09-06 04:58:45 +0300

    add HomeController test methods

--- app/Http/Controllers/Public/HomeController.php
+++ app/Http/Controllers/Public/HomeController.php
@@ -8,6 +8,9 @@ class HomeController extends Controller
 {
     public function index()
     {
-        //
+        // return view('components/footer');
+        // return view('components/navbar');
+        // return view('components/roadmap-card');
+        return view('public/explore');
     }
 }
