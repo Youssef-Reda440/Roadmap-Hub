5803394abcd4262d7d685902638ce662f17a9fa0
Youssef Reda <youssefreda866@gmail.com>
2026-09-06 09:31:21 +0300

    refactor: protect role-based routes

--- routes/creator.php
+++ routes/creator.php
@@ -7,19 +7,25 @@ use App\Http\Controllers\Creator\RoadmapController;
 use App\Http\Controllers\Creator\StatisticsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/creator/dashboard', [DashboardController::class, 'index']);
+Route::middleware(['auth', 'role:creator'])
+    ->prefix('creator')
+    ->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'index']);
 
-Route::get('/creator/roadmaps', [RoadmapController::class, 'index']);
-Route::get('/creator/roadmaps/create', [RoadmapController::class, 'create']);
-Route::post('/creator/roadmaps', [RoadmapController::class, 'store']);
-Route::get('/creator/roadmaps/{roadmap}/edit', [RoadmapController::class, 'edit']);
-Route::patch('/creator/roadmaps/{roadmap}', [RoadmapController::class, 'update']);
-Route::delete('/creator/roadmaps/{roadmap}', [RoadmapController::class, 'destroy']);
-Route::post('/creator/roadmaps/{roadmap}/submit', [RoadmapController::class, 'submit']);
+        Route::prefix('roadmaps')->group(function () {
+            Route::get('/', [RoadmapController::class, 'index']);
+            Route::get('/create', [RoadmapController::class, 'create']);
+            Route::post('/', [RoadmapController::class, 'store']);
+            Route::get('/{roadmap}/edit', [RoadmapController::class, 'edit']);
+            Route::patch('/{roadmap}', [RoadmapController::class, 'update']);
+            Route::delete('/{roadmap}', [RoadmapController::class, 'destroy']);
+            Route::post('/{roadmap}/submit', [RoadmapController::class, 'submit']);
 
-Route::get('/creator/roadmaps/{roadmap}/statistics', [StatisticsController::class, 'show']);
+            Route::get('/{roadmap}/statistics', [StatisticsController::class, 'show']);
+        });
 
-Route::get('/creator/reviews', [ReviewController::class, 'index']);
+        Route::get('/reviews', [ReviewController::class, 'index']);
 
-Route::get('/creator/profile', [ProfileController::class, 'show']);
-Route::patch('/creator/profile', [ProfileController::class, 'update']);
+        Route::get('/profile', [ProfileController::class, 'show']);
+        Route::patch('/profile', [ProfileController::class, 'update']);
+    });
